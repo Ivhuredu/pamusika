753c93bcd9798c721a367c5d179d6d0d4931b1cc
Ivhuredu <belovedwafaith@outlook.com>
2026-02-05 15:58:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -403,25 +403,26 @@ def bot():
     reply = f"📦 Results for '{incoming}':\n\n"
 
     for row in results:
-    product_id, name, price, category, seller, location, phone, photos = row
+         product_id, name, price, category, seller, location, phone, photos = row
 
-    if photos:
-        for img in photos:
-            msg.media(img)
+           if photos:
+            for img in photos:
+                msg.media(img)
 
-    msg.body(
-        f"{seller}\n"
-        f"🛒 {name} ({category})\n"
-        f"💵 {price}\n"
-        f"📍 {location}\n"
-        f"📞 {phone}"
-    )
+        msg.body(
+            f"{seller}\n"
+            f"🛒 {name} ({category})\n"
+            f"💵 {price}\n"
+            f"📍 {location}\n"
+            f"📞 {phone}"
+        )
 
-    msg.body(reply)
-    return str(resp)
+        msg.body(reply)
+        return str(resp)
 
 if __name__ == "__main__":
     app.run()
+
 
 
 
